e523ac5e2ba44ed09d191fe1ed654d3497b0f0cb
Ethan Godsey <egodsey@iwu.edu>
2026-05-01 00:17:15 -0500

    adding images

--- ellie-evens/src/views/HomeView.vue
+++ ellie-evens/src/views/HomeView.vue
@@ -1,9 +1,40 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>{{ name }}</h1>
+    <p>{{ tagline }}</p>
+    <div class="headshot">
+      <img src = "headShotImage">
+    </div>
   </main>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import headShotImage from '@/assets/logo.svg'
+const name = ref('Ellie Evens')
+const tagline = ref('Actor · Singer · Dancer')
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-family: 'Georgia', serif;
+}
+
+h1 {
+  font-size: 3rem;
+  margin: 0;
+}
+
+p {
+  font-size: 1.2rem;
+  color: #666;
+  letter-spacing: 0.15em;
+  text-transform: uppercase;
+  margin-top: 0.5rem;
+}
+</style>
